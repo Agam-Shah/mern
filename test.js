fbcf7d5883d85a0b7dda5c17aae9a5c214281970
Agam-Shah <agammehta93@gmail.com>
2025-11-23 18:05:44 +0530

    add test.js and test.html files

---
+++ test.js
@@ -0,0 +1,20 @@
+console.log("This is a seperate Javascript File.")
+
+let firstName = "Agam";
+let lastName = "Shah";
+
+console.log(firstName + " " + lastName);
+
+let age = 11;
+console.log(age);
+
+age =54;
+console.log(age);
+
+const pi = 3.14;
+//pi = 10; // Throws error because contant variables cannot be changed.
+
+console.log(pi);
+console.log((3+2) - 76 * (1+1))
+
+
